2097a0193376019eb794c970a9720462fb1b4ce1
Matt Sprague <mttsprague@gmail.com>
2025-10-12 21:35:24 -0500

    Initial Commit

---
+++ PolyCal/PolyCalApp.swift
@@ -0,0 +1,32 @@
+//
+//  PolyCalApp.swift
+//  PolyCal
+//
+//  Created by Matthew Sprague on 10/12/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PolyCalApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
